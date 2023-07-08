bbaf6260c230724da8e38f0fa4b09a8732df9b64
Manuj Mishra <manujmishra2000@gmail.com>
2023-07-08 19:39:49 +0100

    Writing to cell

--- src/jupyter.ts
+++ src/jupyter.ts
@@ -1,7 +1,9 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-import { generateCode } from './api';
+import axios from 'axios';
+
+const genCodeEndpoint = 'http://localhost:8080/code';
 
 export const jupyter = async () => {
 
@@ -26,24 +28,50 @@ export async function readFirstMarkdownCell(): Promise<void> {
 
   const notebook: any = parseDocumentToJson(document!);
 
-
   console.log(notebook);
   
-  let foundMarkdown = false;
-  for (let i = 0; i < notebook.cells.length; i++) {
-    if (notebook.cells[i].cell_type === 'markdown') {
-      foundMarkdown = true;
-      continue;
-    }
-
-    if (foundMarkdown && notebook.cells[i].cell_type === 'code') {
-      const cellContent = notebook.cells[i].source.join('').trim();
-      if (!cellContent) {
-        notebook.cells[i].source = ["print(\"hello world\")"];
-      }
-      break;
-    }
+  // Find the first markdown cell that begins with the string "!auto-jupyter" and if none found, return
+  const firstMarkdownCell = notebook.cells.find((cell: any) => {
+    return cell.cell_type === 'markdown' && cell.source[0].trim().startsWith('!auto-jupyter');
+  });
+  if (!firstMarkdownCell) {
+    console.log("No markdown cell found");
+    return;
   }
+
+  // Check if the next cell is a blank code cell and if not, insert a new code cell
+  const blankCodeCell = {
+    cell_type: 'code',
+    execution_count: null,
+    metadata: {},
+    outputs: [],
+    source: [],
+  }
+  if (notebook.cells.indexOf(firstMarkdownCell) === notebook.cells.length - 1) {
+    notebook.cells.push(blankCodeCell);
+  } else if (notebook.cells[notebook.cells.indexOf(firstMarkdownCell) + 1].cell_type !== 'code') {
+    notebook.cells.splice(notebook.cells.indexOf(firstMarkdownCell) + 1, 0, blankCodeCell);
+  } 
+
+  // Get the text after the "!auto-jupyter" string
+  const command = firstMarkdownCell.source[0].trim().split(' ')[1];
+
+
+  // TODO: Delete this bit
+  notebook.cells[notebook.cells.indexOf(firstMarkdownCell) + 1].source = 'example code';
+  return;
+  
+  // Post the command to the endpoint and catch errors
+  let response: string;
+  try {
+    response = (await axios.post(genCodeEndpoint, command)).data.result;
+  } catch (err) {
+    console.error(`Error posting to endpoint: ${err}`);
+    return;
+  }
+
+  // Insert the body of the result into the code cell
+  notebook.cells[notebook.cells.indexOf(firstMarkdownCell) + 1].source = response;
 
   const newFileContent = JSON.stringify(notebook, null, 2);
   fs.writeFileSync(document.uri.fsPath, newFileContent, 'utf8');
