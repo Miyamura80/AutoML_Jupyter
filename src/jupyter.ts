e1cc6fd4f3dedb15e9703961e7926f40b481e9e5
Miyamura80 <eitomiyamura@gmail.com>
2023-07-10 07:53:05 +0100

    Merge conflict with jupyter axios response

--- src/jupyter.ts
+++ src/jupyter.ts
@@ -1,18 +1,20 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-import axios from 'axios';
+import { ServerConnection, Kernel, ServiceManager } from '@jupyterlab/services';
+import { spawn } from 'child_process';
+import { URL } from 'url';
+import axios, { AxiosResponse } from 'axios';
 
 const genCodeEndpoint = 'http://localhost:8080/code';
 
+
 export const jupyter = async () => {
-
   await readFirstMarkdownCell();
-
 };
 
 export async function initialWrite(code: string): Promise<void> {
-  console.log('dummy write')
+  console.log('dummy write');
 }
 
 export async function readFirstMarkdownCell(): Promise<void> {
@@ -23,6 +25,73 @@ export async function readFirstMarkdownCell(): Promise<void> {
     return;
   }
 
+  const editorPath = vscode.window.activeTextEditor.document.uri.fsPath;
+
+  console.log("Server connection");
+  // Start a new Jupyter server
+  let jupyter = spawn('jupyter', ['notebook', '--no-browser']);
+
+  let baseUrl: string | undefined;
+  let token: any;
+  // Handle output - for some weird reason, it runs on stderr
+  jupyter.stderr.on('data', data => {
+    let output = data.toString();
+    console.log('Getting data from stderr containing info:', output);
+
+    // Parse output for URL and token
+    let urlMatch = output.match(/http:\/\/localhost:\d+\/\?token=\w+/);
+    if (urlMatch) {
+      let url = new URL(urlMatch[0]);
+      baseUrl = url.origin;
+      token = url.searchParams.get('token');
+      console.log('Jupyter server base URL:', baseUrl);
+      console.log('Jupyter server token:', token);
+
+      // Call the openAndExecuteCell function after getting the token
+      openAndExecuteCell(baseUrl, token, editorPath);
+    }
+  });
+
+  // Handle stdout
+  jupyter.stdout.on('data', data => {
+    console.log('Jupyter Server threw stout:', data.toString());
+  });
+
+  // Check if the process exited normally
+  jupyter.on('exit', code => {
+    console.log("process exited")
+    if (code !== 0) {
+      console.log(`Jupyter Eito process exited with code ${code}`);
+    }
+  });
+
+  console.log("Server connection");
+  // Create a server settings object
+  let serverSettings: ServerConnection.ISettings = ServerConnection.makeSettings({
+    baseUrl: baseUrl,
+    token: token,
+  });
+
+  console.log("Server connection object created");
+  // Create a service manager
+  let manager: ServiceManager.IManager = new ServiceManager({ serverSettings });
+
+  console.log("Created manager object");
+
+  // Start a new kernel
+  let model: Kernel.IModel;
+  manager.kernelspecs.ready.then(() => {
+    manager.kernels.startNew({ name: 'python3' })  // Change 'python3' to the name of your kernel
+      .then(kernel => {
+        model = kernel.model;
+        console.log('Kernel started:', kernel.id);
+      })
+      .catch(err => {
+        console.error('Failed to start kernel:', err);
+      });
+  });
+
+  console.log("Kernel started");
   // Get the active document
   const document = vscode.window.activeTextEditor?.document;
 
@@ -122,4 +191,61 @@ const parseDocumentToJson = (document: vscode.TextDocument) => {
     return;
   }
   return notebook;
+}
+
+
+interface Cell {
+  id: string;
+  source: string[];
+}
+
+interface NotebookContent {
+  cells: Cell[];
+}
+
+interface NotebookResponse {
+  data: {
+    content: NotebookContent;
+    id: string;
+  }
+}
+
+interface ExecutionResponse {
+  data: any;
+}
+
+async function openAndExecuteCell(baseUrl: string, token: string | null, filepath: string): Promise<void> {
+  try {
+
+      // Name of the notebook
+      // const notebookName: string = 'test.ipynb';
+      const notebookName: string = filepath;
+      console.log('Notebook name:', notebookName);
+
+      // Get the content of the existing notebook
+      const response: AxiosResponse<NotebookResponse> = await axios.get(`${baseUrl}/api/contents${notebookName}`, {
+          headers: {
+              'authorization': `token ${token}`
+          }
+      });
+
+      // Get the cell id of the cell to execute
+      const cellId: string = response.data.data.content.cells[0].id; // Replace 0 with the index of the cell you want to execute
+
+      // Execute the cell
+      const execResponse: AxiosResponse<ExecutionResponse> = await axios.post(`${baseUrl}/api/sessions/${response.data.data.id}/execute`, {
+          code: response.data.data.content.cells[0].source.join('\n'),
+          silent: false,
+          store_history: true,
+          stop_on_error: true
+      }, {
+          headers: {
+              'authorization': `token ${token}`
+          }
+      });
+
+      console.log('Cell execution result:', execResponse.data);
+  } catch (error) {
+      console.error('Error executing cell:', error);
+  }
 }
